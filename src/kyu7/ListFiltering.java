9cdb1770375fb780834462ffb5720ba2ced47a3b
Petr Kukrál <petr.kukral@centrum.cz>
2023-05-18 11:24:39 +0200

    ListFiltering is done

---
+++ src/kyu7/ListFiltering.java
@@ -0,0 +1,11 @@
+package kyu7;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ListFiltering {
+    public static List<Object> filterList(final List<Object> list) {
+        // Return the List with the Strings filtered out
+        return list.stream().filter(Integer.class::isInstance).collect(Collectors.toList());
+    }
+}
