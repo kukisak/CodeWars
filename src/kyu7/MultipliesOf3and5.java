fe1b3575da9d15b0e879fc31cd6df8c31c6f708b
Petr Kukrál <petr.kukral@centrum.cz>
2023-05-20 16:30:25 +0200

    MultipliesOf3and5 is done

---
+++ src/kyu7/MultipliesOf3and5.java
@@ -0,0 +1,18 @@
+package kyu7;
+
+import java.util.stream.IntStream;
+
+public class MultipliesOf3and5 {
+    public int solution(int number) {
+        if (number <= 0) {
+            return 0;
+        }
+        return IntStream.range(3, number)
+                .filter(val -> isMultiplyOf(val, 3) || isMultiplyOf(val, 5))
+                .sum();
+    }
+
+    public boolean isMultiplyOf(int testedNumber, int multiplyBy) {
+        return testedNumber % multiplyBy == 0;
+    }
+}
---
+++ test/kyu7/MultipliesOf3and5Test.java
@@ -0,0 +1,14 @@
+package kyu7;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MultipliesOf3and5Test {
+    @Test
+    public void test() {
+        assertEquals(23, new MultipliesOf3and5().solution(10));
+    }
+
+
+}
